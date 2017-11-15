4977c0b801de7b5f2cc983a26f778db7dc399a8a
nanoTech Corporation <thiagofigcosta@hotmail.com>
2017-11-15 11:37:29 -0200

    Machine

--- SnoopDog.v
+++ SnoopDog.v
@@ -9,88 +9,117 @@ module SnoopDog();
 
 endmodule
 
-module machine(clock,write,read,state, read_miss_in, write_miss_in, invalidate_in, shared);
-input clock, write, read, read_miss_in, write_miss_in, shared;
-output reg[1:0] state;
-output reg read_miss_out, invalidate_out, write_miss_out, writeback, abort_memory;
+module MESI(clock,op_in,miss_in,inv_in,state,new_state,
+	        read_hit_out,read_miss_out,write_hit_out,write_miss_out,invalidate_out,invalidade_out,write_back_out,abort_out);
 
-assign read_hit = ~read_miss_in;
-assign write_hit = ~write_miss_in;
-assign not_shared= ~shared;
+input clock;
+input  op_in,miss_in,inv_in,state;
+
+output reg read_hit_out,read_miss_out,write_hit_out,write_miss_out,invalidate_out,invalidade_out,write_back_out,abort_out,new_state;
+
+wire read_hit,write_hit,read_miss,write_miss
+
+assign read_hit = ~op_in & ~miss_in;
+assign write_hit = op_in & ~miss_in;
+assign read_miss = ~op_in & miss_in;
+assign write_miss = op_in & miss_in;
 
 
 always@(posedge clock)
-read_miss_out=0;
-invalidate_out=0;
-write_miss_out=0; 
-writeback=0;
-abort_memory=0;
-case(state)
-	0://invalid
-	begin
-	 if(read & shared)
-	 begin
-		state=1;
-		read_miss_out=1;
-	 end
-	 if(read & not_shared)
-	 begin
-		read_miss_out=1;
-		state=3;
-	 end
-	 if(write)
-	 begin
-		state=2;
-		write_miss_out=1;
-	 end
-	end
-	1://Shared
-	begin
-	if((write & write_miss_in) | (invalidate_in & !write & !read ))
-	 begin
-		state=0;
-	 end
-	 if(write)
-	 begin
-		invalidate=1;
-		state=2;
-	 end
-	end
-	2://Modified
-	begin
-		if(write_miss_in & write)
-		 begin
-			writeback=1;
-			abort_memory=1;
-			state=0;
-		 end
-		 if(read_miss_in & read)
-		 begin
-			writeback=1;
-			abort=1;
-			state=1;
-		 end
-		end
-	end
-	3://Excl
-	begin
-		if(write_miss_in | invalidate_in)
-		begin
-			state=0;
-		end
-		if(write_hit & write)
-		begin
-			state=2;
-		end
-		if(read & read_miss_in)
-		begin
-			state=1;
-		end
-	end
-endcase
+begin
+	read_hit_out=0;
+	read_miss_out=0;
+	write_hit_out=0;
+	write_miss_out=0;
+	invalidate_out=0;
+	invalidade_out=0;
+	write_back_out=0;
+	abort_out=0;
 
-endmodule 
+	case(state)
+		0://invalidade
+		begin
+			if (op_in) // write
+			begin
+				new_state=2;
+				write_miss_out=1;
+			end
+			else  // read
+			begin
+				new_state=1;
+				read_miss_out=1;	
+			end
+		end
 
+
+		1://shared
+		begin
+			if(read_hit) begin 
+				read_hit_out=1;
+			end
+			else if(write_hit) begin 
+				write_hit_out=1;
+				invalidate_out=1;
+				new_state=2;
+			end
+			else if(read_miss) begin 
+				read_miss_out=1;
+			end
+			else if(write_miss | inv_in) begin 
+				write_miss_out=1;
+				invalidate_out=1;
+				new_state=0;
+			end
+		end
+
+
+		2://modified
+		begin
+			if(read_hit) begin 
+				read_hit_out=1;
+			end
+			else if(write_hit) begin 
+				write_hit_out=1;
+			end
+			else if(read_miss) begin 
+				read_miss_out=1;
+				abort_out=1;
+				write_back_out=1;
+				new_state=1;
+			end
+			else if(write_miss) begin 
+				write_miss_out=1;
+				abort_out=1;
+				write_back_out=1;
+				new_state=0;
+			end
+		end
+
+
+		3://exclusive
+		begin
+			if(read_hit) begin 
+				read_hit_out=1;
+			end
+			else if(write_hit) begin 
+				write_hit_out=1;
+				new_state=2;
+			end
+			else if(read_miss) begin 
+				read_miss_out=1;
+				abort_out=1;
+				write_back_out=1;
+				new_state=1;
+			end
+			else if(write_miss | inv_in) begin 
+				write_miss_out=1;
+				abort_out=1;
+				write_back_out=1;
+				new_state=0;
+			end
+		end
+	endcase
+end
 
 module cache();
 	reg[] tag   [3:0];
