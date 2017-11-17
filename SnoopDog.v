cdd7134a33e563b6850afe8973395ed8fa00a820
Bernard2254 <bernard_menezes@hotmail.com>
2017-11-17 08:19:20 -0200

    Cria Toplevel parte 1
    Cria toplevel para simular a máquina de estados na placa. Prepara modulo processador para seu toplevel.

--- SnoopDog.v
+++ SnoopDog.v
@@ -1,4 +1,8 @@
-module SnoopDog();
+module SnoopDog(KEY[3:0], SW[17:0], LEDR[17:0]);
+input [17:0]SW;
+input [3:0]KEY;
+
+output [17:0] LEDR;
 
 //chaves 17 16 15 selecionam o processador
 //HEX 0 1 2 indicam o estado da maquina ed estados de cada 1
@@ -7,13 +11,28 @@ module SnoopDog();
 //chaves 2-6 selecionam o endereco
 //chaves 7-14 valor a gravar
 
+MESI teste  (KEY[0]			//clock
+			,SW[2]			//write
+			,SW[3]			//miss
+			,SW[4]			//invalidate
+			,SW[1:0]		//state
+
+			,LEDR[1:0]		//new state
+			,LEDR[17]		//read hit
+			,LEDR[16]		//read miss
+			,LEDR[15]		//write hit
+			,LEDR[14]		//write miss
+			,LEDR[13]		//invalidate out
+			,LEDR[12]		//writeback out
+			,LEDR[11]);		//abort acess memory
+
 endmodule
 
 module MESI(clock,op_in,miss_in,inv_in,state,
 			new_state,read_hit_out,read_miss_out,write_hit_out,write_miss_out,invalidate_out,write_back_out,abort_out);
 
 input clock;
-input op_in,miss_in,inv_in,
+input op_in,miss_in,inv_in, // op_in = write
 input[1:0] state;
 
 output reg read_hit_out,read_miss_out,write_hit_out,write_miss_out,invalidate_out,write_back_out,abort_out;
@@ -184,13 +203,33 @@ module processor(clock,snooping,instr,data_out,in,out);
 	assign addr = instr[5:1];
 	assign value = instr[13:6];
 
-	cache l1(clock,addr,block_hit,block_state,block_val);
-	MESI machine(clock,op,~block_hit, 0 ,block_state, new_state, read_hit,read_miss,write_hit,write_miss,invalidate,write_back,abort);
+	cache l1(clock 				// Clock 				Entrada
+			,addr				// Endereço 			Entrada
+
+			,block_hit			// Hit 					Saída
+			,block_state		// Situação do bloco	Saída
+			,block_val);		// Valor encontrado		Saída
+
+	MESI machine(clock 			// Clock 				Entrada
+				,op 			// Write 				Entrada
+				,~block_hit		// Miss 				Entrada
+				, 0 			// Invalidate 			Entrada
+				,block_state 	// Situação do bloco 	Entrada
+
+				, new_state		// Nova situação 		Saída
+				, read_hit 		// Read hit 			Saída
+				,read_miss 		// Read miss 			Saída
+				,write_hit 		// Write hit 			Saída
+				,write_miss 	// Write miss 			Saída
+				,invalidate 	// Invalidate 			Saída
+				,write_back 	// Write back 			Saída
+				,abort); 		// Aborta memoria 		Saída
+
 	MESI snoopMachine(clock,op, in[1]&in[3],in[0],block_state, snoop_state, 0,0,0,0,0,0,0);
 
 	always @(posedge clock)
 	begin
-		out[0]=invalidade;
+		out[0]=invalidate;
 		out[1]=read_miss|write_miss;
 		out[2]=write_back; // tem que gravar na memoria o valor !!!!!!!!!!!!
 		out[3]=abort;
